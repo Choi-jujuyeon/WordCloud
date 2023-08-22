4d62dec8b7d095e7339cffad199faae3558c0088
Choi-jujuyeon <chlwndus1216@naver.com>
2023-08-23 00:50:54 +0900

    connect firebase-react

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import AppShell from "./AppShell";
+import Home from "./Home";
+import Texts from "./Texts";
+import Words from "./Words";
+
 class App extends React.Component {
     render() {
-        return <AppShell />;
+        return (
+            <Router>
+                <AppShell>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/texts" element={<Texts />} />
+                        <Route path="/words" element={<Words />} />
+                    </Routes>
+                </AppShell>
+            </Router>
+        );
     }
 }
+
 export default App;
--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,17 +1,13 @@
 import React from "react";
-import {
-    WithStyles,
-    AppBar,
-    Drawer,
-    MenuItem,
-    IconButton,
-    withStyles,
-} from "@material-ui/core/";
-// import { AppBar } from "@material-ui/core/AppBar";
-// import { Drawer } from "@material-ui/core/Drawer";
-// import { MenuItem } from "@material-ui/core/MenuItem";
-// import { IconButton } from "@material-ui/core/IconButton";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@material-ui/core/Link";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Drawer from "@material-ui/core/Drawer";
+import MenuItem from "@material-ui/core/MenuItem";
+import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+
 const styles = {
     root: {
         flexGrow: 1,
@@ -20,6 +16,7 @@ const styles = {
         marginRight: "auto",
     },
 };
+
 class AppShell extends React.Component {
     constructor(props) {
         super(props);
@@ -31,20 +28,38 @@ class AppShell extends React.Component {
     render() {
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <IconButton
-                        className={classes.menuButton}
-                        color="inherit"
-                        onClick={this.handleDrawerToggle}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                </AppBar>
-                <Drawer open={this.state.toggle}>
-                    <MenuItem onClick={this.handleDrawerToggle}>Home1</MenuItem>
-                    <MenuItem onClick={this.handleDrawerToggle}>Home2</MenuItem>
-                </Drawer>
+            <div>
+                <div className={classes.root}>
+                    <AppBar position="static">
+                        <IconButton
+                            className={classes.menuButton}
+                            color="inherit"
+                            onClick={this.handleDrawerToggle}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </AppBar>
+                    <Drawer open={this.state.toggle}>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/">
+                                Home
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/texts">
+                                Texts
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/words">
+                                Words
+                            </Link>
+                        </MenuItem>
+                    </Drawer>
+                </div>
+                <div id="content" style={{ margin: "auto", marginTop: "20px" }}>
+                    {React.cloneElement(this.props.children)}
+                </div>
             </div>
         );
     }
